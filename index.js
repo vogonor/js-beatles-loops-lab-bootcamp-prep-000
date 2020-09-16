de78d75eacc2897040c8fd957441c9ae102887de
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-16 23:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function johnLennonFacts(johnFacts) {
   while(factsCount < johnFacts.length){
     jFact = johnFacts[factsCount] + "!!!"
     newFacts.push(jFact)
+    factsCount--
     
   }
   return newFacts
