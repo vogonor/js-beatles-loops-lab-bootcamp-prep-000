1383bb3b3d4e7ebe2d270afb40ce0dccf78dc2de
observant-algorithm-6319 <vogonor@yahoo.com>
2020-09-16 22:36:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   const musicArr = []
+  
+  for(var i = 0; i<musician.length; i++){
+    var arrContent = muician[i] + " "+ instrument[i]
+  }
 }
